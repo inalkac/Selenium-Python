97d05bf8f5287c3b7e25091195155c5b90fabe26
Leyla <leylyam@gmail.com>
2021-07-18 15:51:53 +0300

    edited

--- locators/pdp_locators.py
+++ locators/pdp_locators.py
@@ -1,5 +1,6 @@
 from selenium.webdriver.common.by import By
 
 class PDPLocators():
-    variations = "variations"
-    submit_btn = (By.XPATH,'//button[@type="submit"]')
+    variations_id = "variations"
+    submit_btn = "//button[@type='submit']"
+    selector_attr = '//select[@data-buy-box-select='
